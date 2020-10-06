2f37fb518eed561510fcc12e9a34b6a2c151bfd8
Tom Anderson <azazil47@gmail.com>
2020-10-06 15:44:46 +0700

    Пиздец изменения

--- Form1.cs
+++ Form1.cs
@@ -15,8 +15,27 @@ namespace DBMaster
 {
     public partial class Form1 : Form
     {
-        public List<String[]> listService = new List<String[]>();
+        public static List<String[]> listService = new List<String[]>();
+        public static void LoadSrvice() //Загрузка списка служб из файла
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("Services.ini"))
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        listService.Add(new string[2] { line, ServiceClass.Status(line) });
+                    }
+                }
+                MessageBox.Show("Load is good");
+            }
+            catch (Exception)
+            {
 
+                MessageBox.Show("File is bad");
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -26,60 +45,63 @@ namespace DBMaster
         private void button2_Click(object sender, EventArgs e) //Status
         {
             //serv.Refresh();
-           // MessageBox.Show(serv.Status().ToString());
+            // MessageBox.Show(serv.Status().ToString());
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-           // serv.Stop();
+            ServiceClass.Stop("FirebirdServerDefaultInstance");
+            dataGridView1.Refresh();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            listService.Add(new string[2] { "FirebirdServerDefaultInstance", $"{ServiceClass.Status("FirebirdServerDefaultInstance")}" });
-            listService.Add(new string[2] { "spooler", $"{ServiceClass.Status("spooler")}"});
+            LoadSrvice();
+            // listService.Add(new string[2] { "FirebirdServerDefaultInstance", $"{ServiceClass.Status("FirebirdServerDefaultInstance")}" });
+            // listService.Add(new string[2] { "spooler", $"{ServiceClass.Status("spooler")}"});
             foreach (String[] item in listService)
             {
                 dataGridView1.Rows.Add(item);
             }
-            
+
 
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-           // serv.Start();
+            ServiceClass.Start("FirebirdServerDefaultInstance");
+            dataGridView1.Refresh();
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-           /* BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream("Services.dat", FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, serv);
-                MessageBox.Show("Serializeble is good");
-            }
+            /* BinaryFormatter formatter = new BinaryFormatter();
+             using (FileStream fs = new FileStream("Services.dat", FileMode.OpenOrCreate))
+             {
+                 formatter.Serialize(fs, serv);
+                 MessageBox.Show("Serializeble is good");
+             }
 
-            XmlSerializer formatter2 = new XmlSerializer(typeof(ServiceClass));
-            using (FileStream fs2 = new FileStream("Sevices.xml", FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs2, serv);
-                MessageBox.Show("Serializeble is good XML");
-            }*/
+             XmlSerializer formatter2 = new XmlSerializer(typeof(ServiceClass));
+             using (FileStream fs2 = new FileStream("Sevices.xml", FileMode.OpenOrCreate))
+             {
+                 formatter.Serialize(fs2, serv);
+                 MessageBox.Show("Serializeble is good XML");
+             }*/
         }
 
         private void button2_Click_1(object sender, EventArgs e)
         {
-           /* BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream("Services.dat", FileMode.OpenOrCreate))
-            {
-                ServiceClass serv = (ServiceClass)formatter.Deserialize(fs);
-                MessageBox.Show("DeSerializeble is good");
-               
-                
-            }*/
+            /* BinaryFormatter formatter = new BinaryFormatter();
+             using (FileStream fs = new FileStream("Services.dat", FileMode.OpenOrCreate))
+             {
+                 ServiceClass serv = (ServiceClass)formatter.Deserialize(fs);
+                 MessageBox.Show("DeSerializeble is good");
+
+
+             }*/
         }
     }
 }
