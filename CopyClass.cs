536aa8187cee19aac4b92406e6b9fa2f6e85edb9
Azazil47 <72367218+Azazil47@users.noreply.github.com>
2020-10-25 22:05:56 +0700

    еще изменения

--- CopyClass.cs
+++ CopyClass.cs
@@ -66,7 +66,7 @@ namespace DBMaster
                     hash2 = md5.ComputeHash(stream);
                 }
             }
-            if (hash1.ToString()==(hash2.ToString()))
+            if (hash1.ToString().Equals(hash2.ToString()))
             {
                 MessageBox.Show("База данных успешно скопирована");
             }
